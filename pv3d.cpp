7c6e1c13fb8e51a78c9110e1a6a053ce810b87ec
Josh Vita <vita.joshua@gmail.com>
2017-07-18 10:53:02 -0700

    typedef'd vector<double> = doublev_t

--- pv3d.cpp
+++ pv3d.cpp
@@ -13,8 +13,9 @@
 #include <cmath>
 
 using namespace std;
+typedef vector<double> doublev_t;
 
-vector<vector<double> > dot(vector<vector<double> > A, vector<vector<double> > B) {
+vector<doublev_t> dot(vector<doublev_t> A, vector<doublev_t> B) {
     /* Calculates the dot product of two matrices.
      *
      * Args:
@@ -28,13 +29,13 @@ vector<vector<double> > dot(vector<vector<double> > A, vector<vector<double> > B
     int rows = A.size();
     int cols = B[0].size();
 
-    vector<vector<double> > result;
+    vector<doublev_t> result;
     result.reserve(rows);
 
     double sum;
 
     for (int i=0; i<rows; i++) {
-        vector<double> temp;
+        doublev_t temp;
         temp.reserve(cols);
 
         for (int j=0; j<cols; j++) {
@@ -50,7 +51,7 @@ vector<vector<double> > dot(vector<vector<double> > A, vector<vector<double> > B
     return result;
 }
 
-vector<double> dot(vector<vector<double> > A, vector<double> B) {
+doublev_t dot(vector<doublev_t> A, doublev_t B) {
     /* Calculates the dot product of two matrices. Function overloading for use
      * with 1D right matrix.
      *
@@ -65,7 +66,7 @@ vector<double> dot(vector<vector<double> > A, vector<double> B) {
     int rows = A.size();
     int cols = B.size();
 
-    vector<double> result;
+    doublev_t result;
     result.reserve(rows);
 
     double sum;
@@ -82,21 +83,22 @@ vector<double> dot(vector<vector<double> > A, vector<double> B) {
 
     return result;
 }
-void printArr(vector<vector<double> > arr) {
+
+void printArr(vector<doublev_t> arr) {
     /* Prints a 2D array to the console. */
 
     int len = arr.size();
     int wid = arr[0].size();
 
-    for (vector<vector <double> >::size_type i=0; i<len; ++i) {
-        for (vector<vector<double> >::size_type j=0; j<wid; ++j){
+    for (vector<vector <double>>::size_type i=0; i<len; ++i) {
+        for (vector<doublev_t>::size_type j=0; j<wid; ++j){
             cout << arr[i][j] << " ";
         }
         cout << endl;
     }
 }
 
-vector<vector<double> > rotate(vector<vector<double> > arr, double theta, vector<double> axis) {
+vector<doublev_t> rotate(vector<doublev_t> arr, double theta, doublev_t axis) {
     /* Rotates a multidimensional array about a given axis by a given theta.
      * Uses the Rodrigues rotation formula.
      *
@@ -106,62 +108,43 @@ vector<vector<double> > rotate(vector<vector<double> > arr, double theta, vector
      *  axis    - axis of rotation
      */
 
-    // Initialize the K matrix
-    //double matrixK[3][3] = {{0,-axis[0],axis[1]},
-    //                        {axis[2],0,-axis[0]},
-    //                        {-axis[1],axis[0],0}};
-    //
-
     int numPoints = arr.size();
 
-    vector<vector<double> > matrixK = {{0,-axis[2],axis[1]},
+    // Initialize the K matrix
+    vector<doublev_t> matrixK = {{0,-axis[2],axis[1]},
                                         {axis[2],0,-axis[0]},
                                         {-axis[1],axis[0],0}};
 
-    //cout << "matrixK:" << endl;
-    //printArr(matrixK);
-
-    vector<vector<double> > KK;
+    vector<doublev_t> KK;
     KK.reserve(3);
 
     KK = dot(matrixK, matrixK);
 
-    //cout << "KK:" << endl;
-    //printArr(KK);
-
-    vector<vector<double> > rotMat;
+    vector<doublev_t> rotMat;
     rotMat.reserve(3);
 
-    double tmp;
     double val;
 
     // Initialize the rotation matrix
     for (int i=0; i<3; i++) {
-        vector<double> temp_v;
+        doublev_t temp_v;
         temp_v.reserve(3);
 
         for (int j=0; j<3; j++) {
-            tmp = matrixK[i][j];
-            
-            val = tmp*sin(theta) + KK[i][j]*(1-cos(theta));
+            val = matrixK[i][j]*sin(theta) + KK[i][j]*(1-cos(theta));
 
             if (i==j)
                 val++;
 
             temp_v.push_back(val);
         }
-        //cout << "adding temp_v of size: " << temp_v.size() << endl;
         rotMat.push_back(temp_v);
-        //cout << "new size: " << rotMat.size() << endl;
     }
 
-    //cout << "rotMat:" << endl;
-    //printArr(rotMat);
-
-    vector<vector<double> > output;
+    vector<doublev_t> output;
     output.reserve(numPoints);
 
-    for (vector<vector<double> >::size_type i=0; i<numPoints; ++i) {
+    for (vector<doublev_t>::size_type i=0; i<numPoints; ++i) {
         output.push_back(dot(rotMat, arr[i]));
     }
 
@@ -170,22 +153,22 @@ vector<vector<double> > rotate(vector<vector<double> > arr, double theta, vector
 
 int main() {
 
-    vector<double> axis;
+    doublev_t axis;
 
-    vector<vector<double> > test_v;
+    vector<doublev_t> test_v;
 
     // TEST 1: z-axis rotation
     axis = {0,0,1};
-    test_v.push_back(vector<double> {0,0,0});
-    test_v.push_back(vector<double> {0,-1,0});
-    test_v.push_back(vector<double> {-1,0,0});
-    test_v.push_back(vector<double> {0,1,0});
-    test_v.push_back(vector<double> {1,0,0});
+    test_v.push_back(doublev_t {0,0,0});
+    test_v.push_back(doublev_t {0,-1,0});
+    test_v.push_back(doublev_t {-1,0,0});
+    test_v.push_back(doublev_t {0,1,0});
+    test_v.push_back(doublev_t {1,0,0});
 
     cout << "Before:" << endl;
     printArr(test_v);
 
-    vector<vector<double> > output;
+    vector<doublev_t> output;
     output = rotate(test_v, M_PI/4, axis);
 
     cout << "After:" << endl;
@@ -194,11 +177,11 @@ int main() {
     // TEST 2: y-axis rotation
     axis = {0,1,0};
     test_v.clear();
-    test_v.push_back(vector<double> {0,0,0});
-    test_v.push_back(vector<double> {-1,0,0});
-    test_v.push_back(vector<double> {1,0,0});
-    test_v.push_back(vector<double> {0,0,-1});
-    test_v.push_back(vector<double> {0,0,1});
+    test_v.push_back(doublev_t {0,0,0});
+    test_v.push_back(doublev_t {-1,0,0});
+    test_v.push_back(doublev_t {1,0,0});
+    test_v.push_back(doublev_t {0,0,-1});
+    test_v.push_back(doublev_t {0,0,1});
 
     cout << "Before:" << endl;
     printArr(test_v);
@@ -211,11 +194,11 @@ int main() {
     // TEST 3: x-axis rotation
     axis = {1,0,0};
     test_v.clear();
-    test_v.push_back(vector<double> {0,0,0});
-    test_v.push_back(vector<double> {0,1,0});
-    test_v.push_back(vector<double> {0,-1,0});
-    test_v.push_back(vector<double> {0,0,-1});
-    test_v.push_back(vector<double> {0,0,1});
+    test_v.push_back(doublev_t {0,0,0});
+    test_v.push_back(doublev_t {0,1,0});
+    test_v.push_back(doublev_t {0,-1,0});
+    test_v.push_back(doublev_t {0,0,-1});
+    test_v.push_back(doublev_t {0,0,1});
 
     cout << "Before:" << endl;
     printArr(test_v);
@@ -228,11 +211,11 @@ int main() {
     // TEST 3: arbitrary axis rotation
     axis = {1,1,1};
     test_v.clear();
-    test_v.push_back(vector<double> {0,0,0});
-    test_v.push_back(vector<double> {1,0,0});
-    test_v.push_back(vector<double> {0,1,0});
-    test_v.push_back(vector<double> {0,0,1});
-    test_v.push_back(vector<double> {1,1,1});
+    test_v.push_back(doublev_t {0,0,0});
+    test_v.push_back(doublev_t {1,0,0});
+    test_v.push_back(doublev_t {0,1,0});
+    test_v.push_back(doublev_t {0,0,1});
+    test_v.push_back(doublev_t {1,1,1});
 
     cout << "Before:" << endl;
     printArr(test_v);
